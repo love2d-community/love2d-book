1965058055307e983c991dd29940d768829bd0d9
S0lll0s <s0lll0s@blinkenshell.org>
2015-04-12 02:12:35 +0200

    Allow passing flags to render.sh

--- render.sh
+++ render.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p tmp
-./asciidoctor book/html.adoc -o public/index.html
-./asciidoctor book/pdf.adoc -o public/love2D-book.pdf -b pdf
+./asciidoctor book/html.adoc -o public/index.html "$@"
+./asciidoctor book/pdf.adoc -o public/love2D-book.pdf -b pdf "$@"
 
 #mkdir -p public/paged
 #for f in book/world*/*.adoc; do
